d0715adda148a4cae16c2b8b0234202487e4e4d1
Agustín <amarquardt@movizen.com>
2024-04-29 01:09:41 -0300

    update app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './@components/product-list/product-list.component';
+import { ProductDetailComponent } from './@components/product-detail/product-detail.component';
 
 export const routes: Routes = [
     { path: '', component: ProductListComponent },
-    // { path: 'product/:id', component: ProductDetailsComponent },
+    { path: 'product/:id', component: ProductDetailComponent },
 ];
